fbfd66631f97d11f5b7239a18b4be589b17c06c8
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-11-10 11:59:10 -0800

    Final working version

---
+++ JaeProject/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace JaeProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 411);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 260);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ConvertToPixels";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(547, 23);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(480, 260);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(547, 287);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(480, 260);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(262, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Convert To Pixels - 2nd Method";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.convert2ndMethod_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 581);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ JaeProject/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JaeProject
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap membmp;
+        List<Bitmap> imagesCollected = new List<Bitmap>(NUMBER_OF_FILES);
+        List<Bitmap> imagesProcessed = new List<Bitmap>(NUMBER_OF_FILES);
+
+        Bitmap example = new Bitmap("Images/dog.jpg");
+
+        String[] files = new String[NUMBER_OF_FILES];
+        int currentFile = 0;
+        const int NUMBER_OF_FILES = 728;
+        const int WIDTH = 176;
+        const int HEIGHT = 144;
+
+        int N = 1 + (int)Math.Log(HEIGHT, 2);
+
+
+        Thread t;
+        System.Windows.Forms.Timer UpdateTimer;
+        delegate void PixelFunc(int x, int y, Color c);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            files = Directory.GetFiles("OriginalVideoFrames/")
+                                    .Select(path => Path.GetFileName(path))
+                                    .ToArray();
+
+            for (int image = 0; image < files.Length; image++)
+            {
+                imagesCollected.Add(new Bitmap("OriginalVideoFrames/" + files[image]));
+            }
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for(int i =0; i< 176; i++)
+            {
+                imagesProcessed[i].Save(Application.StartupPath + "/ProcessedVideo/img" + i + ".png" , 
+                                        ImageFormat.Png);
+
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //int imageCounter = 0;
+
+            for (int column = 0; column < 176; column++)
+            {
+                Bitmap imgLocal = new Bitmap(758, 144);
+
+                for (int line = 0; line < 144; line++)
+                {
+                    for (int image = 0; image < files.Length; image++)
+                    {
+                        imgLocal.SetPixel(image, line, imagesCollected[image].GetPixel(column, line));
+                    }
+                }
+                imagesProcessed.Add(imgLocal);
+                
+            }
+
+
+
+            //pictureBox1.Image = imagesProcessed[0];
+            //pictureBox2.Image = imagesProcessed[20];
+            //pictureBox3.Image = imagesProcessed[49];
+
+            /*for(int i=0; i<NUMBER_OF_FILES;i++)
+            {
+                pictureBox1.Image = imagesProcessed[i];
+
+            }*/
+        }
+
+        private void convert2ndMethod_Click(object sender, EventArgs e)
+        {
+            
+            int[ ,] oldHistogram = new int[N+1 , N+1];
+            Bitmap imgResult = new Bitmap( files.Length, WIDTH + 1 );
+
+            for (int image = 0; image < files.Length; image++)
+            {
+
+                for (int column = 0; column < 176; column++)
+                {
+                    int[,] histogram = new int[N + 1, N + 1];
+
+                    for (int line = 0; line < 144; line++)
+                    {
+                        Bitmap imgLocal = imagesCollected[image];
+
+                        Color pixel = imgLocal.GetPixel(column, line);
+
+
+                       
+                      
+                        if ((int)pixel.R + (int)pixel.G + (int)pixel.B == 0)
+                        {
+                            histogram[0, 0]++;
+                        }
+                        else
+                        {
+                            double valueR = ((double)pixel.R / (pixel.R + pixel.G + pixel.B)) *N;
+                            double valueG = ((double)pixel.G / (pixel.R + pixel.G + pixel.B)) * N;
+
+                            int pixelR = (int) valueR ;
+                            int pixelG = (int) valueG ;
+
+                            histogram[pixelR, pixelG]++;
+                        }
+                        //imgLocal.SetPixel(image, line, pixel);
+                    }
+
+                    if (column == 0)
+                    {
+                        oldHistogram = histogram;
+                    }
+
+                    //Intersecting the two histograms
+                    double summedNumber = 0;
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            summedNumber += (double)(Math.Min(histogram[i, j], oldHistogram[i, j])) / WIDTH;
+                        }
+
+                    }
+
+                    oldHistogram = histogram;
+
+                    Color finalPixel;
+                    if (summedNumber == 0)
+                    {
+                        finalPixel = Color.FromArgb(0, 0, 0);
+                    }
+                    else
+                    {
+                        int summedNumberInt = (int)Math.Floor(summedNumber * 255);
+                        if (summedNumberInt > 255)
+                        {
+                            summedNumberInt = 255;
+                        }
+                        finalPixel = Color.FromArgb(summedNumberInt, summedNumberInt, summedNumberInt);
+                    }
+
+                    imgResult.SetPixel(image, column, finalPixel);
+                }
+
+
+            }
+            imgResult.Save(Application.StartupPath + "/ProcessedVideo/part2.png",
+                                        ImageFormat.Png);
+
+
+            for(int i=0; i<files.Length; i++)
+            {
+                for(int j=0;j<WIDTH + 1; j++)
+                {
+                    if(imgResult.GetPixel(i,j).B>150)
+                    {
+                        Color thresholdPixel = Color.FromArgb(255, 255, 255);
+                        imgResult.SetPixel(i, j, thresholdPixel);
+                    }
+                    else
+                    {
+                        Color thresholdPixel = Color.FromArgb(0, 0, 0);
+                        imgResult.SetPixel(i, j, thresholdPixel);
+                    }
+                }
+            }
+
+            imgResult.Save(Application.StartupPath + "/ProcessedVideo/part2Threshold.png",
+                            ImageFormat.Png);
+        }
+
+    }
+}
